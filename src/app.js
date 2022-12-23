42c8ae03d7de0c3ec720319b9c2655a546e0db4b
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-23 14:36:35 -0600

    set up express to work with hbs and serve files

--- src/app.js
+++ src/app.js
@@ -9,7 +9,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Brayan Garcia',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Brayan Garcia',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Screen',
+    message: 'This is some helpful text.',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
