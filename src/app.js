78a838a05afe0c3b96ea41b27f1f10f89b3032bf
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-24 17:28:40 -0600

    understand query strings

--- src/app.js
+++ src/app.js
@@ -45,9 +45,31 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if(!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  }
+
   res.send({
     forecast: 'It is sunny',
     location: 'Santa Ana, Santa Ana, El Salvador',
+    address: req.query.address,
+  });
+});
+
+app.get('/products', (req, res) => {
+
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term',
+    });
+  }
+
+  console.log(req.query.search);
+
+  res.send({
+    products: [],
   });
 });
 
