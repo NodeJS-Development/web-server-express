69ed2529892336d78b5f4ec261717c6abd3817c5
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-24 18:08:09 -0600

    fix /weather logic and response

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 // console.log(__dirname);
 // console.log(__filename);
 // console.log(path.join(__dirname, '../public'));
@@ -45,17 +48,38 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  if(!req.query.address) {
+  if (!req.query.address) {
     return res.send({
       error: 'You must provide an address',
     });
   }
 
-  res.send({
-    forecast: 'It is sunny',
-    location: 'Santa Ana, Santa Ana, El Salvador',
-    address: req.query.address,
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error,
+      });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        res.send({
+          error
+        });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+
+    });
+
   });
+
+
+
 });
 
 app.get('/products', (req, res) => {
